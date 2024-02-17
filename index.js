b1743f1fdffb2f4a1795823f364200d544a8b740
Pavendra Singh Rajput <127604065+Pavendra21@users.noreply.github.com>
2024-02-17 12:33:47 +0530

    images added

---
+++ index.js
@@ -0,0 +1,15 @@
+// For Search box
+
+function toggleSearchBox() {
+    var searchBox = document.getElementById('searchBox');
+    if (searchBox.style.display === 'none' || searchBox.style.display === '') {
+        searchBox.style.display = 'block';
+    } else {
+        searchBox.style.display = 'none';
+    }
+}
+
+function performSearch() {
+    // Add your search functionality here
+     
+}
